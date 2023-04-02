4c7b44e33c1a05b5061fe76dcd99c98088b75426
Ayan Sultan <oiansheks@gmail.com>
2023-04-02 20:16:12 +0600

    Add User model, UserManager, Dockerfile, docker-compose.yml

---
+++ chat/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser):
+
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    def get_full_name(self):
+        return {
+            self.first_name,
+            self.last_name
+        }
+
+    def __str__(self):
+        return self.email
