53419b42ee724187709162b48db1013a1ad94142
Ayan Sultan <oiansheks@gmail.com>
2023-06-01 20:26:47 +0600

    dumb(): dumb commit

--- chat/serializers.py
+++ chat/serializers.py
@@ -6,35 +6,5 @@ from django.contrib.auth.password_validation import validate_password
 from chat.models import *
 
 
-class UserSerializer(serializers.ModelSerializer):
-
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-    )
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
-    re_password = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name',
-                  'password', 're_password')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['re_password']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
-
-        return attrs
-
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-
-        return instance
+class RegistrationSerializer(serializers.Serializer):
+    pass
--- chat/views.py
+++ chat/views.py
@@ -2,16 +2,3 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
 
 from chat.models import User
-from chat.serializers import UserSerializer
-
-
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def get_permissions(self):
-        if self.action in ['update', 'partial_update', 'destroy']:
-            permission_classes = [permissions.IsAuthenticated]
-        else:
-            permission_classes = [permissions.AllowAny]
-        return [permission() for permission in permission_classes]
